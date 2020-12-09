5d0ee7fc7a9a7bfbdf14c36df6d8ae8c802c78ac
Giulio <giuliovolletti@hotmail.com>
2020-12-09 16:04:02 +0100

    commit2

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ var slide = new Vue (
   {
     el: '.html',
     data: {
-      message: "hello world"
+      imgUrl: ['https://theculturetrip.com/wp-content/uploads/2017/12/34131593773_0fba25e24e_k.jpg', 'https://pix10.agoda.net/hotelImages/4888945/0/d1a3d1a932eba94dd2f2158aa66bf3ee.jpg?s=1024x768','https://cdn.getyourguide.com/img/location/5ca345852da87.jpeg/148.jpg', 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'],
+    },
+    methods: {
+      toLeft: function (){
+        console.log("left");
+      },
+      toRight: function (){
+        console.log("right");
+      }
     }
   }
 );
